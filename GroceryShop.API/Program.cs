f016ba237748ac4ce208c649f3c143d6624ff554
Sergey Vasiliev <aragamerek@gmail.com>
2024-03-13 15:47:32 +0300

    feat: add dependencies in pipeline

--- GroceryShop.API/Program.cs
+++ GroceryShop.API/Program.cs
@@ -1,10 +1,21 @@
+using GroceryShop.API.Extensions;
+using GroceryShop.API.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.ConfigureControllersAndFilters();
+builder.Services.ConfigureMiddlewares();
+builder.Services.ConfigureDbContext();
+builder.Services.ConfigureRepositoryManager();
+builder.Services.ConfigureMapper();
+builder.Services.ConfigureServiceManager();
+builder.Services.ConfigureAuth(builder.Configuration);
+builder.Services.ConfigureSwagger();
+builder.Logging.ConfigureLogger();
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 if (app.Environment.IsDevelopment())
 {
@@ -12,10 +23,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
